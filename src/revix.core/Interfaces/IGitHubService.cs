3675a9ecf25b0d62b9ed2f7d434a73729bbe11df
omkarpatil14 <omkarpatil9881@gmail.com>
2026-03-08 01:49:29 +0530

    added feature to add comments in PR section

--- src/revix.core/Interfaces/IGitHubService.cs
+++ src/revix.core/Interfaces/IGitHubService.cs
@@ -7,4 +7,6 @@ public interface IGitHubService
         string repo,
         int prNumber,
         string accessToken);
+
+    Task PostReviewCommentAsync(string owner, string repo, int prNumber, string body, string accessToken);
 }
